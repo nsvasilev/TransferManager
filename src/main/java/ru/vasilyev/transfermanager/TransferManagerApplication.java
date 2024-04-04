741735bf1ead9f5db1fe253b6deab57d31d7e52f
Nikita Vasilyev <Nik.vasilev.20@inbox.ru>
2024-04-04 11:43:12 +0300

    создал вотч сервис

--- src/main/java/ru/vasilyev/transfermanager/TransferManagerApplication.java
+++ src/main/java/ru/vasilyev/transfermanager/TransferManagerApplication.java
@@ -2,12 +2,13 @@ package ru.vasilyev.transfermanager;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
 
+
+@EnableAsync
 @SpringBootApplication
 public class TransferManagerApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(TransferManagerApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(TransferManagerApplication.class, args);
+    }
 }
--- src/main/java/ru/vasilyev/transfermanager/beans/WatchServiceConfig.java
+++ src/main/java/ru/vasilyev/transfermanager/beans/WatchServiceConfig.java
@@ -1,2 +1,43 @@
-package ru.vasilyev.transfermanager.beans;public class WatchServiceConfig {
-}
+package ru.vasilyev.transfermanager.beans;
+
+import jakarta.annotation.PostConstruct;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+import java.nio.file.*;
+
+
+
+@Configuration
+@Component
+public class WatchServiceConfig {
+    @PostConstruct
+    @Async
+    public void watchService() throws IOException, InterruptedException {
+        WatchService watchService
+                = FileSystems.getDefault().newWatchService();
+
+        Path path = Paths.get("C:\\Users\\nikva\\OneDrive\\Рабочий стол\\java projects\\watchFile");
+
+        path.register(
+                watchService,
+                StandardWatchEventKinds.ENTRY_CREATE,
+                StandardWatchEventKinds.ENTRY_DELETE,
+                StandardWatchEventKinds.ENTRY_MODIFY);
+
+        WatchKey key;
+        while ((key = watchService.take()) != null) {
+            for (WatchEvent<?> event : key.pollEvents()) {
+                System.out.println(
+                        "Event kind:" + event.kind()
+                                + ". File affected: " + event.context() + ".");
+            }
+            key.reset();
+        }
+    }
+
+    }
+
