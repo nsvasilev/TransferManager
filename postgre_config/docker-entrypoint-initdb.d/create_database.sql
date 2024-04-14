1dee9d2f74590c7c0af8a7ba01fd0c116f113460
romanovio <romanovio@vtb.ru>
2024-04-14 18:52:20 +0300

    Правки

--- postgre_config/docker-entrypoint-initdb.d/create_database.sql
+++ postgre_config/docker-entrypoint-initdb.d/create_database.sql
@@ -3,10 +3,10 @@ CREATE DATABASE bank_users OWNER nikita_admin;
 \c bank_users
 
 CREATE TABLE bank_user(
-    id serial not null,
+    id serial PRIMARY KEY,
     lastname varchar(64) not null,
     firstname varchar(64) not null,
     patronymic varchar(64),
     gender varchar(6) not null,
-    birthDate date not null
+    birth_date date not null
 );
--- src/main/java/ru/vasilyev/transfermanager/entity/BankUserEntity.java
+++ src/main/java/ru/vasilyev/transfermanager/entity/BankUserEntity.java
@@ -12,14 +12,16 @@ import java.time.LocalDate;
 @NoArgsConstructor
 @Getter
 @Setter
+@Table(name= "bank_user")
 public class BankUserEntity {
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String firstname;
     private String lastname;
     private String patronymic;
     private String gender;
+    @Column(name = "birth_date")
     private LocalDate birthDate;
 
     public BankUserEntity(String firstname, String lastname, String patronymic, String gender, LocalDate birthDate) {
