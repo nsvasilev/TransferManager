9cd54d11685095479f008f6aac710f0b47850d90
Nikita Vasilyev <Nik.vasilev.20@inbox.ru>
2024-04-24 16:03:03 +0300

    правки и комментарии внесены.

---
+++ src/main/java/ru/vasilyev/transfermanager/interfaces/FileProcessService.java
@@ -0,0 +1,11 @@
+package ru.vasilyev.transfermanager.interfaces;
+
+/**
+ * todo Внедрение паттернов: command, chains of responsibility,
+ * todo Соблюдение принципа open close principle
+ *
+ */
+public interface FileProcessService {
+
+    String Extension();
+}
